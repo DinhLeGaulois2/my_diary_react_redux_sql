ff9de6800efda7c9f96b74228adb08b9b956f238
DinhLeGaulois2 <dinh.hu87@yahoo\.com>
2019-12-01 11:09:05 -0800

    Schema + Models

--- server/models/index.js
+++ server/models/index.js
@@ -27,20 +27,13 @@ db.Sequelize = Sequelize;
 db.addresses = require('./addresses.js')(sequelize, Sequelize)
 db.diary = require('./diary.js')(sequelize, Sequelize)
 db.diary_events = require('./diary_events.js')(sequelize, Sequelize)
-db.diary_kws = require('./diary_kws.js')(sequelize, Sequelize)
 db.diary_persons = require('./diary_persons.js')(sequelize, Sequelize)
 db.diary_self = require('./diary_self.js')(sequelize, Sequelize)
 db.emails = require('./emails.js')(sequelize, Sequelize)
 db.events = require('./events.js')(sequelize, Sequelize)
-db.kws = require('./kws.js')(sequelize, Sequelize)
-db.person_addrs = require('./person_addrs.js')(sequelize, Sequelize)
-db.person_emails = require('./person_emails.js')(sequelize, Sequelize)
-db.person_phones = require('./person_phones.js')(sequelize, Sequelize)
-db.person_socialMedias = require('./person_socialMedias.js')(sequelize, Sequelize)
 db.persons = require('./persons.js')(sequelize, Sequelize)
 db.phones = require('./phones.js')(sequelize, Sequelize)
 db.self = require('./self.js')(sequelize, Sequelize)
-db.socialMedias = require('./socialMedias.js')(sequelize, Sequelize)
 db.users = require('./users.js')(sequelize, Sequelize)
 db.users_diary = require('./users_diary.js')(sequelize, Sequelize)
 //=========================================================================================================//
@@ -52,9 +45,6 @@ db.diary_events.belongsTo(db.diary)
 
 db.diary.hasMany(db.diary_self, { onDelete: 'CASCADE', hooks: true })
 db.diary_self.belongsTo(db.diary)
-
-db.diary.hasMany(db.diary_kws, { onDelete: 'CASCADE', hooks: true })
-db.diary_kws.belongsTo(db.diary)
 
 db.diary.hasMany(db.users_diary, { onDelete: 'CASCADE', hooks: true })
 db.users_diary.belongsTo(db.diary)
@@ -68,32 +58,20 @@ db.person_emails.belongsTo(db.emails)
 db.events.hasMany(db.diary_events, { onDelete: 'CASCADE', hooks: true })
 db.diary_events.belongsTo(db.events)
 
-db.kws.hasMany(db.diary_kws, { onDelete: 'CASCADE', hooks: true })
-db.diary_kws.belongsTo(db.kws)
-
 db.persons.hasMany(db.diary_persons, { onDelete: 'CASCADE', hooks: true })
 db.diary_persons.belongsTo(db.persons)
 
-db.persons.hasMany(db.person_socialMedias, { onDelete: 'CASCADE', hooks: true })
-db.person_socialMedias.belongsTo(db.persons)
+db.persons.hasMany(db.addresses, { onDelete: 'CASCADE', hooks: true })
+db.addresses.belongsTo(db.persons)
 
-db.persons.hasMany(db.person_emails, { onDelete: 'CASCADE', hooks: true })
-db.person_emails.belongsTo(db.persons)
+db.persons.hasMany(db.emails, { onDelete: 'CASCADE', hooks: true })
+db.emails.belongsTo(db.persons)
 
-db.persons.hasMany(db.person_addrs, { onDelete: 'CASCADE', hooks: true })
-db.person_addrs.belongsTo(db.persons)
-
-db.persons.hasMany(db.person_phones, { onDelete: 'CASCADE', hooks: true })
-db.person_phones.belongsTo(db.persons)
-
-db.phones.hasMany(db.person_phones, { onDelete: 'CASCADE', hooks: true })
-db.person_phones.belongsTo(db.phones)
+db.persons.hasMany(db.phones, { onDelete: 'CASCADE', hooks: true })
+db.phones.belongsTo(db.persons)
 
 db.self.hasMany(db.diary_self, { onDelete: 'CASCADE', hooks: true })
 db.diary_self.belongsTo(db.self)
-
-db.socialMedias.hasMany(db.person_socialMedias, { onDelete: 'CASCADE', hooks: true })
-db.person_socialMedias.belongsTo(db.socialMedias)
 
 db.users.hasMany(db.users_diary, { onDelete: 'CASCADE', hooks: true })
 db.users_diary.belongsTo(db.users)
--- server/routes/api-routes-read.js
+++ server/routes/api-routes-read.js
@@ -6,4 +6,37 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const db = require("../models");
 
 module.exports = function (app) {
+    app.get("/api/get/diary/all/:userLoginId", requireAuth, (req, res) => {
+        db.users.findAll({
+            where: { loginId: req.params.userLoginId },
+            include: [
+                {
+                    model: db.users_diary,
+                    include: [
+                        {
+                            model: db.diary,
+                            include: [
+                                {
+                                    model: db.diary_events,
+                                    include: [{ model: db.events, attributes: ['id', 'title'] }]
+                                },
+                                {
+                                    model: db.diary_self,
+                                    include: [{ model: db.self }]
+                                },
+                                {
+                                    model: db.diary_kws,
+                                    include: [{ model: db.kws }]
+                                },
+                                { model: db.persons }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        })
+            .then(userRes => {
+                res.status(200).json(userRes)
+            }).catch(err => res.status(400).json(err))
+    })
 }
